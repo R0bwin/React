69eebfa5c4635a9b5ae25f37200a7fe8645bdab0
R0bwin <67636136+R0bwin@users.noreply.github.com>
2020-10-21 11:53:01 +0000

    Deploying to gh-pages from github-pages @ 0da6d899976809d1d20fc39325bed0b6764e0080 🚀

---
+++ precache-manifest.f7bd34ff5638cda303d17cc96dce010a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebf67a367c6e39b2bc0672c3ce0b6240",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "f395ae88b343a99592fb",
+    "url": "/React/static/css/main.9312ee1e.chunk.css"
+  },
+  {
+    "revision": "d0486bea29d82598153c",
+    "url": "/React/static/js/2.f2c396b3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React/static/js/2.f2c396b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f395ae88b343a99592fb",
+    "url": "/React/static/js/main.9a20019a.chunk.js"
+  },
+  {
+    "revision": "f0430320babf4fb19fa7",
+    "url": "/React/static/js/runtime-main.fc7effbd.js"
+  }
+]);
